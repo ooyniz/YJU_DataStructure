7da58580b1a4787c18dc8aad746f663c09e588af
유진 <102000749+ooyniz@users.noreply.github.com>
2022-09-21 14:39:16 +0900

    Create Max.java

---
+++ Chapter01/Max.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Max {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b, c, max;
+		System.out.println("�� �Է� ");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		sc.close();
+		max = a;
+		if (b > max)
+			max = b;
+		if (c > max)
+			max = c;
+		System.out.println(max);
+	}
+
+}
